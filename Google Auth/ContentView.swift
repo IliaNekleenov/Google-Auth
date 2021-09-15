a9df893076cee91776759d307f662b942e6b5fa3
ruanei5 <neiluha@mail.ru>
2021-09-15 23:12:07 +0300

    Initial Commit

---
+++ Google Auth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Google Auth
+//
+//  Created by NEKLEENOV Ilya on 15.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Google Auth/Google_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_AuthApp.swift
+//  Google Auth
+//
+//  Created by NEKLEENOV Ilya on 15.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Google_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
